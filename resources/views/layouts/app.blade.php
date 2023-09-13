3728dc40c5bfd1e85a6be5b2488555e45f759c78
7noel <noel.logan@gmail.com>
2023-09-13 15:54:48 -0500

    111

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,12 @@
     if (null == session('my_company')) {
         session(['my_company' => App\Modules\Finances\Company::find(1)]);
     }
-    dd(\Storage::url( session('my_company')->config['favicon']));
+    //dd(\Storage::url( session('my_company')->config['favicon']));
 @endphp
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @if(get_defined_vars(session('my_company')->config['favicon']) and \Storage::disk('public')->exists(session('my_company')->config['favicon']) )
-    <link rel="icon" type="image/jpeg" href="/img/favicon.png" />
-    @else
-    <link rel="icon" type="image/jpeg" href="/img/favicon.png" />
-    @endif    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -120,12 +115,8 @@
         <nav class="{{ config('options.styles.navbar') }}">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    @if(get_defined_vars(session('my_company')->config['logo']) and \Storage::disk('public')->exists(session('my_company')->config['logo']) )
-                    <img src="{{ \Storage::url( session('my_company')->config['logo']) }}" alt="" height="50px">
-                    @else
                     <!-- <img src="/img/logo_makim_doc.jpg" alt="" height="50px"> -->
                     {{ config('app.name', 'Laravel') }}
-                    @endif
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
